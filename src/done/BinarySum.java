71f6264be7a0f1c87baee72541eb423561acc902
Angélica Martins <marttinsangelica@gmail.com>
2024-09-08 11:34:10 -0300

    fea: add new challenge

---
+++ src/done/BinarySum.java
@@ -0,0 +1,24 @@
+package done;
+
+/*
+  Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+  The binary number returned should be a string.
+
+  Examples:(Input1, Input2 --> Output (explanation)))
+
+  1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+  5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+*/
+
+public class BinarySum {
+
+  public static void main(String[] args) {
+    System.out.println(binaryAddition(1, 1));
+  }
+
+  public static String binaryAddition(int a, int b) {
+    return Integer.toBinaryString(a + b);
+  }
+
+}
