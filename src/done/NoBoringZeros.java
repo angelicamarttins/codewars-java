8145d8288ea8a50fa1ff5b9ae28609810b9ecbc7
Angélica Martins <marttinsangelica@gmail.com>
2024-09-17 09:22:48 -0300

    fea: add new challenge

--- src/done/NoBoringZeros.java
+++ src/done/NoBoringZeros.java
@@ -2,6 +2,20 @@ package done;
 
 public class NoBoringZeros {
 
+  /*
+    Numbers ending with zeros are boring.
+
+    They might be fun in your world, but not here.
+
+    Get rid of them. Only the ending ones.
+
+    1450 -> 145
+    960000 -> 96
+    1050 -> 105
+    -1050 -> -105
+    Zero alone is fine, don't worry about it. Poor guy anyway
+  */
+
   public static void main(String[] args) {
     System.out.println(noBoringZeros(1450));
     System.out.println(noBoringZeros(1050));
@@ -10,11 +24,18 @@ public class NoBoringZeros {
   }
 
   private static int noBoringZeros(int number) {
-    while (number % 10 == 0) {
-      number /= 10;
+    if (number == 0) {
+      return 0;
     }
 
-    return number;
+    String numberStr = Integer.toString(number);
+
+    int endIndex = numberStr.length();
+    while (endIndex > 0 && numberStr.charAt(endIndex - 1) == '0') {
+      endIndex--;
+    }
+
+    return Integer.parseInt(numberStr.substring(0, endIndex));
   }
 
 }
