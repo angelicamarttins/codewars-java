1b0d063843091bd667359acbbb45e2eaf673bc6c
Angélica Martins <marttinsangelica@gmail.com>
2024-09-22 20:47:08 -0300

    fea: add new challenge

---
+++ src/done/DuplicateEncoder.java
@@ -0,0 +1,39 @@
+package done;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class DuplicateEncoder {
+  private static String newString = "";
+
+  public static void main(String[] args) {
+    //System.out.println(encode("Prespecialized"));
+    System.out.println(encode("   ()(   "));
+  }
+
+  public static String encode(String string) {
+    Map<String, Integer> map = new HashMap<>();
+    newString = string.toLowerCase();
+
+    Arrays.asList(string.toLowerCase().split("")).forEach(letter -> {
+      map.merge(letter, 1, Integer::sum);
+    });
+
+    map.forEach((key, value) -> {
+      System.out.println(key + " " + value);
+      if (value > 1) {
+        System.out.println("mais um");
+        newString = newString.replaceAll(Pattern.quote(key), ")");
+        System.out.println(newString);
+      } else {
+        System.out.println("um");
+        newString = newString.replaceAll(Pattern.quote(key), "(");
+      }
+    });
+    System.out.println(map);
+
+    return newString;
+  }
+}
