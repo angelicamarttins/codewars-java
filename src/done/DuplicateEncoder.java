6b6d0b2c6fd7e4416b810ef9a1c7e44e60b9dd35
Angélica Martins <marttinsangelica@gmail.com>
2024-09-28 10:58:37 -0300

    fea: add new challenge

--- src/done/DuplicateEncoder.java
+++ src/done/DuplicateEncoder.java
@@ -1,45 +1,64 @@
 package done;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class DuplicateEncoder {
-  private static String newString = "";
+
+  /*
+    The goal of this exercise is to convert a string to a new string where each character in the new
+    string is "(" if that character appears only once in the original string, or ")" if that character
+    appears more than once in the original string. Ignore capitalization when determining if a character
+    is a duplicate.
+
+    Examples
+    "din"      =>  "((("
+    "recede"   =>  "()()()"
+    "Success"  =>  ")())())"
+    "(( @"     =>  "))(("
+    Notes
+    Assertion messages may be unclear about what they display in some languages.
+    If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+ */
+
 
   public static void main(String[] args) {
-    //System.out.println(encode("Prespecialized"));
+    System.out.println(encode("Prespecialized"));
     System.out.println(encode("   ()(   "));
   }
 
-  public static String encode(String string) {
-    Map<String, Integer> map = new HashMap<>();
-    newString = string.toLowerCase();
+  public static String encode(String word) {
+    Map<String, List<Integer>> mappedString = new HashMap<>();
+    List<String> listString = Arrays.asList(word.toLowerCase().split(""));
 
-    Arrays.asList(string.toLowerCase().split("")).forEach(letter -> {
-      map.merge(letter, 1, Integer::sum);
-    });
+    IntStream.range(0, listString.size()).forEach(index -> mappedString.merge(
+        listString.get(index),
+        mappingFn(true, index, null),
+        (existingList, newList) -> mappingFn(false, index, existingList)
+    ));
 
-    map.forEach((key, value) -> {
-      System.out.println(key + " " + value);
-      if (value > 1) {
-        System.out.println("mais um");
-        newString = newString.replaceAll(Pattern.quote(key), ")");
-        System.out.println(newString);
+    mappedString.forEach((key, value) -> IntStream.range(1, value.size()).forEach(value1 -> {
+      if (value.get(0) > 1) {
+        listString.set(value.get(value1), ")");
       } else {
-        System.out.println("um");
-        newString = newString.replaceAll(Pattern.quote(key), "(");
+        listString.set(value.get(value1), "(");
       }
-    });
-    System.out.println(map);
+    }));
 
-    return newString;
+    return String.join("", listString);
+  }
+
+  private static List<Integer> mappingFn(boolean first, int index, List<Integer> list) {
+    if (first) {
+      List<Integer> newList = new ArrayList<>();
+      newList.add(1);
+      newList.add(index);
+      return newList;
+    }
+
+    Integer position = list.get(0);
+    list.set(0, position + 1);
+    list.add(index);
+    return list;
   }
 }
-
-/*
-* Criar outro map, dentro do map, colocando a quantidade de aparições e as posições de cada aparição
-* Daí, buscar pela chave do primeiro map e dentro do segundo map, buscar pela letra e alterar nas
-* posições em que aparece a letra na string original adicionando ( ou )
-*/
