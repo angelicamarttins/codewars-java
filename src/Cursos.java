038dbae495b84044b49f7538a3cd457589ff7fdc
Angélica Martins <marttinsangelica@gmail.com>
2024-10-13 10:15:16 -0300

    fea: add new challenge

---
+++ src/Cursos.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Cursos {
+  public static void main(String[] args) {
+    var dataFinal = LocalDate.now().plusDays(43);
+    System.out.println(dataFinal);
+  }
+}
